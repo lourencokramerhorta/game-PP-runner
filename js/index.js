22b3fda82bbb7a25386810173592c924380e7cd7
UmaCouve <lourencokramerhorta@gmail.com>
2020-11-24 19:39:29 +0000

    added files and folders

---
+++ js/index.js
@@ -0,0 +1,40 @@
+class Game {
+  constructor() {}
+}
+
+class Player extends Game {
+    constructor() {
+        this.height = x;
+        this.width = x;
+        this.speed = x;
+        this.x = x;
+        this.y = x;
+        this.ratio = x;
+        this.img = x;
+    }
+    drawPlayer();
+    movePlayer();
+    abilitie();
+}
+class Crowd extends Game {
+    constructor() {
+        this.height = x;
+        this.width = x;
+        this.x = x;
+        this.y = x;
+        this.img = x;
+    }
+    drawCrowd();
+}
+class PowerUp extends Game {
+    constructor() {
+        this.height = x;
+        this.width = x;
+        this.x = x;
+        this.y = x;
+        this.img = x;
+    }
+    drawPowerUp();
+}
+
+function startGame() { };
